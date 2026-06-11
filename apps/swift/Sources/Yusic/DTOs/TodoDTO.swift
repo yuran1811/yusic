70a8490f4d37203b12ffc4c1124c82dfe34ffb18
yuran1811 <trieuvanbd123@gmail.com>
2026-06-11 12:53:43 +0700

    feat: add Swift configuration and update project structure

--- apps/swift/Sources/Yusic/DTOs/TodoDTO.swift
+++ apps/swift/Sources/Yusic/DTOs/TodoDTO.swift
@@ -2,16 +2,16 @@ import Fluent
 import Vapor
 
 struct TodoDTO: Content {
-    var id: UUID?
-    var title: String?
-    
-    func toModel() -> Todo {
-        let model = Todo()
-        
-        model.id = self.id
-        if let title = self.title {
-            model.title = title
-        }
-        return model
+  var id: UUID?
+  var title: String?
+
+  func toModel() -> Todo {
+    let model = Todo()
+
+    model.id = self.id
+    if let title = self.title {
+      model.title = title
     }
+    return model
+  }
 }
--- apps/swift/Sources/Yusic/Models/Todo.swift
+++ apps/swift/Sources/Yusic/Models/Todo.swift
@@ -1,29 +1,30 @@
 import Fluent
+
 import struct Foundation.UUID
 
 /// Property wrappers interact poorly with `Sendable` checking, causing a warning for the `@ID` property
 /// It is recommended you write your model with sendability checking on and then suppress the warning
 /// afterwards with `@unchecked Sendable`.
 final class Todo: Model, @unchecked Sendable {
-    static let schema = "todos"
-    
-    @ID(key: .id)
-    var id: UUID?
+  static let schema = "todos"
 
-    @Field(key: "title")
-    var title: String
+  @ID(key: .id)
+  var id: UUID?
 
-    init() { }
+  @Field(key: "title")
+  var title: String
 
-    init(id: UUID? = nil, title: String) {
-        self.id = id
-        self.title = title
-    }
-    
-    func toDTO() -> TodoDTO {
-        .init(
-            id: self.id,
-            title: self.$title.value
-        )
-    }
+  init() {}
+
+  init(id: UUID? = nil, title: String) {
+    self.id = id
+    self.title = title
+  }
+
+  func toDTO() -> TodoDTO {
+    .init(
+      id: self.id,
+      title: self.$title.value
+    )
+  }
 }
