b23b877f54ca01401caa9b5598501dd05b687982
yuran1811 <trieuvanbd123@gmail.com>
2026-06-11 14:35:59 +0700

    feat: enhance swift code

--- apps/swift/Sources/Yusic/Middleware/RateLimiter.swift
+++ apps/swift/Sources/Yusic/Middleware/RateLimiter.swift
@@ -1,14 +1,42 @@
-import NIOConcurrencyHelpers
 import Vapor
 
-final class RateLimiterMiddleware: AsyncMiddleware {
-  private let lock = NIOLock()
+actor RateLimiterStorage {
   private var requests: [String: [Date]] = [:]
+
+  func isAllowed(
+    ip: String,
+    now: Date,
+    maxRequests: Int,
+    window: TimeInterval
+  ) -> Bool {
+    var timestamps = requests[ip] ?? []
+
+    timestamps.removeAll {
+      now.timeIntervalSince($0) > window
+    }
+
+    guard timestamps.count < maxRequests else {
+      requests[ip] = timestamps
+      return false
+    }
+
+    timestamps.append(now)
+    requests[ip] = timestamps
+
+    return true
+  }
+}
+
+final class RateLimiterMiddleware: AsyncMiddleware {
+  private let storage = RateLimiterStorage()
 
   private let maxRequests: Int
   private let window: TimeInterval
 
-  init(maxRequests: Int, window: TimeInterval) {
+  init(
+    maxRequests: Int,
+    window: TimeInterval
+  ) {
     self.maxRequests = maxRequests
     self.window = window
   }
@@ -18,24 +46,13 @@ final class RateLimiterMiddleware: AsyncMiddleware {
     chainingTo next: any AsyncResponder
   ) async throws -> Response {
     let ip = request.remoteAddress?.ipAddress ?? "unknown"
-    let now = Date()
 
-    let allowed = lock.withLock {
-      var timestamps = requests[ip] ?? []
-
-      timestamps.removeAll {
-        now.timeIntervalSince($0) > window
-      }
-
-      guard timestamps.count < maxRequests else {
-        return false
-      }
-
-      timestamps.append(now)
-      requests[ip] = timestamps
-
-      return true
-    }
+    let allowed = await storage.isAllowed(
+      ip: ip,
+      now: Date(),
+      maxRequests: maxRequests,
+      window: window
+    )
 
     guard allowed else {
       throw Abort(
--- apps/swift/Sources/Yusic/routes.swift
+++ apps/swift/Sources/Yusic/routes.swift
@@ -14,7 +14,19 @@ func routes(_ app: Application) throws {
     let name = try req.parameters.require("name")
 
     let number = Int.random(in: 1...100)
-    req.logger.info("The number is: \(number)")
+    let quotation = """
+      Even though there's whitespace to the left,
+      the actual lines aren't indented.
+          Except for this line.
+      Double quotes (") can appear without being escaped.
+
+      I still have \(number) pieces of fruit.
+      """
+    req.logger.info("The quotation is: \(quotation)")
+
+    let numbers = [1, 2, 3, 4, 5]
+    let trippleNumbers = numbers.map { $0 * 3 }
+    req.logger.info("The numbers are: \(numbers), and the tripple numbers are: \(trippleNumbers)")
 
     return "Hello, \(name.capitalized)!"
   }
